70c975b5d86ffe190366f7d71554511aec0064ed
Muklesur Rahman <rmuklesur1231@gmail.com>
2021-09-20 18:56:29 +0800

    second commit

--- bookdb.js
+++ bookdb.js
@@ -1,15 +1,34 @@
-
 // --------------Search information Loading-------------
+
 const searchBook=()=>{
     const searchField=document.getElementById('search_field');
+    const errorMessage=document.getElementById("error_msg");
     const searchText=searchField.value;
     searchField.value='';
+
+    //  document.getElementById("Error_msg").style.display="none";
+      if (searchText === '') {   
+        const div= document.createElement('div');
+         div.innerHTML=`
+         <div>
+        <p class="text-center text-danger fs-2"> !! Please Type your book name!!</p>
+        </div>
+         `;
+         errorMessage.appendChild(div);
+         
+        
+      }
+    
+      else {
+        
 
     const url_1=`https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url_1)
     .then(res=>res.json())
     .then(data=>displaySearch(data))
+    document.getElementById("error_msg").innerHTML="";
    }
+}
 // -----------Search Result-----------
 const displaySearch=(data)=>{
     const searchResultInput=document.getElementById('search-result');
@@ -18,7 +37,7 @@ const displaySearch=(data)=>{
     searchBookDetails.forEach(searchResult=>{
 // --------------Search items-------------------------
     const bookCoverImage=searchResult.cover_i;
-    const bookName=searchResult.subject;
+    const bookName=searchResult.title;
     const firstPublish=searchResult.first_publish_year;
     const publishDate=searchResult.publish_date;
     const author_name=searchResult.author_name;
